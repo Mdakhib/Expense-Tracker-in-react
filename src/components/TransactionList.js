94f93b33f5b45124d4340477871cfe6ec88668aa
Mdakhib <mohamedakhib@gmail.com>
2021-08-22 10:50:49 +0530

    transaction data is added by using props

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
+
 
 function TransactionList() {
+  const { transaction } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transaction.map((transaction) => (
+          <Transaction transaction={transaction} key={transaction.id} />
+        ))}
       </ul>
     </>
   );
