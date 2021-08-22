0b71377ffbf96f38def916f8aa91023952f06a6e
Mdakhib <mohamedakhib@gmail.com>
2021-08-22 11:33:53 +0530

    total income and balance is done by using reduce and filter methods

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function Balance() {
-    return (
-      <>
-        <h4>Your Balance</h4>
-        <h1>$0.00</h1>
-      </>
-    );
+  const { transaction } = useContext(GlobalContext);
+
+  const amounts = transaction.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${total}</h1>
+    </>
+  );
 }
 
-export default Balance
+export default Balance;
